1c2378a9001bd3c7ca09593ea0eaa74bc5be6c78
Yash Gurjar <ys.gurjar@gmail.com>
2024-04-19 19:01:33 +0000

    create first view

--- expense_manager_app/home_page/views.py
+++ expense_manager_app/home_page/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+# -1 Import http response object
+from django.http import HttpResponse
+
 # Create your views here.
+
+
+# -2 Create your function, that returns an http response
+def index(request):
+    return HttpResponse("This works!")
