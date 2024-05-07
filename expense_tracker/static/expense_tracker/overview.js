30c63b6bcd3c9bc23f56ce4f2e70bb9b513be9eb
Yash Gurjar <ys.gurjar@gmail.com>
2024-05-07 14:49:22 +0000

    create total income and total expense chart

--- expense_tracker/static/expense_tracker/overview.js
+++ expense_tracker/static/expense_tracker/overview.js
@@ -1,8 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
+
     // Access the JSON data stored by Django in the HTML
     const rawData = document.getElementById('mydata').textContent;
-    const myData = JSON.parse(rawData);
 
-    // Now you can use `myData` in your JavaScript code
-    console.log(myData);
+    // Need to parse twice to be able to get correct value
+    const myData = JSON.parse(JSON.parse(rawData));
+    const total_income = parseInt(myData.total_income);  // Parse the income to integer
+    const total_expense = parseInt(myData.total_expense);  // Parse the expense to integer
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Total Income', 'Total Expense'],
+        datasets: [{
+            label: 'Financial Overview',
+            data: [total_income, total_expense],  
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.8)',  // Color for Total Income
+                'rgba(255, 99, 132, 0.8)'   // Color for Total Expense
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
