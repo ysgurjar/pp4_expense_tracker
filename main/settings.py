a94a0d5182645a4046f08e6eb35c919b62130440
Yash Gurjar <ys.gurjar@gmail.com>
2024-05-08 23:13:24 +0000

    install whitenoise to debug service static files

--- main/settings.py
+++ main/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'expense_tracker.middleware.BlockAuthURLsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "main.urls"
